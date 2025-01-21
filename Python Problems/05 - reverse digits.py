509cc3b0b5ac59e2a933e5dd43687032ece455b3
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-21 22:31:32 +0600

    Create 05 - reverse digits.py

---
+++ Python Problems/05 - reverse digits.py
@@ -0,0 +1,6 @@
+# Write a program that reverses a four-digit number.
+# Additionally, ensure that it checks whether the reversal is true.
+
+user_input = int(input("Enter the four digit number: "))
+
+num = user_input
