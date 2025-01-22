d4e242afd5af5397c9d9513c7ed32f921c95f70a
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-22 22:34:46 +0600

    Update 09 - 3 angles triangle.py

---
+++ Python Problems/09 - 3 angles triangle.py
@@ -0,0 +1,5 @@
+# Write a program that takes user input for three angles and determines whether they can form a triangle or not.
+
+a = int(input("Enter your first angle: "))   
+b = int(input("Enter your second angle: "))  
+c = int(input("Enter your third angle: "))   
