a25fa2e7fb0ca527c716b390f084440dcc81e008
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-22 22:34:57 +0600

    Update 09 - 3 angles triangle.py

--- Python Problems/09 - 3 angles triangle.py
+++ Python Problems/09 - 3 angles triangle.py
@@ -3,3 +3,4 @@
 a = int(input("Enter your first angle: "))   
 b = int(input("Enter your second angle: "))  
 c = int(input("Enter your third angle: "))   
+if a + b + c == 180 and a != 0 and b != 0 and c != 0:
