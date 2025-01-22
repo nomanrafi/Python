0d9a2c1a649ef6a0c94bd615c50ce44a16846fb8
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-22 22:34:18 +0600

    Update 08 - euclidean dist.py

--- Python Problems/08 - euclidean dist.py
+++ Python Problems/08 - euclidean dist.py
@@ -2,3 +2,10 @@
 
 x_1 = float(input("Enter x1 of x coordinate: "))
 y_1 = float(input("Enter y1 of y coordinate: "))
+
+x_2 = float(input("Enter x2 of x coordinate: "))
+y_2 = float(input("Enter y2 of y coordinate: "))
+
+d = ((x_2 - x_1)**2 + (y_2 - y_1)**2)**0.5
+
+print(d)
