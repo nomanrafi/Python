7e435a38e365f7efea289f0ecc9e55df3bb3ef40
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-23 22:40:27 +0600

    Create 15 - sum of squares.py

---
+++ Python Problems/15 - sum of squares.py
@@ -0,0 +1,8 @@
+# Write a program that takes three digits from the user and adds the square of each digit.
+
+num = int(input("Enter your number: "))
+
+a = num % 10
+num = num // 10
+b = num % 10
+c = num // 10
