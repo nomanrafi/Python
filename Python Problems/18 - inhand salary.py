6369514253715efb42b529cc74e3842c61a33a2a
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-24 21:05:32 +0600

    Update 18 - inhand salary.py

--- Python Problems/18 - inhand salary.py
+++ Python Problems/18 - inhand salary.py
@@ -5,3 +5,13 @@
 # If the salary is between 0-1 lakh, print 'k'.
 
 user_input = float(input('Enter your annual salary: '))
+
+if user_input > 500000 and user_input < 1000000:
+    tax = (10/100) * user_input
+    temp_salary = user_input - tax
+elif user_input > 1000000 and user_input < 2000000:
+    tax = (20/100) * user_input
+    temp_salary = user_input - tax
+else:
+    tax = (30/100) * user_input
+    temp_salary = user_input - tax
