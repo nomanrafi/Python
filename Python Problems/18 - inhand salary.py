69d8d1f29f006b12eb747791414d12960edd2f04
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-24 21:05:19 +0600

    Update 18 - inhand salary.py

---
+++ Python Problems/18 - inhand salary.py
@@ -0,0 +1,7 @@
+# Write a program that calculates the in-hand salary after deducting HRA (10%), DA (5%), PF (3%), and tax.
+# If the salary is between 5-10 lakh, apply a 10% tax;
+# for salaries between 11-20 lakh, apply a 20% tax;
+# for salaries above 20 lakh, apply a 30% tax.
+# If the salary is between 0-1 lakh, print 'k'.
+
+user_input = float(input('Enter your annual salary: '))
