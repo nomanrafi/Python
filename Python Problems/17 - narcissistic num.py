9677e28ecb7bd8e4760380a040954782ce7ce5b5
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-24 21:04:30 +0600

    Update 17 - narcissistic num.py

---
+++ Python Problems/17 - narcissistic num.py
@@ -0,0 +1,5 @@
+# Write a program that will take user input of a (4-digit number) and check whether the number is a narcissist number or not.
+
+user_input = int(input("Enter a four digit number: "))
+
+num = user_input
