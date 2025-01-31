623084e8e0b45ddc04b03493140b20625459a984
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-01-31 20:41:44 +0600

    Update 19 - menu driven.py

---
+++ Python Problems/19 - menu driven.py
@@ -0,0 +1,10 @@
+# Write a menu-driven program with the following options:
+
+# 1. cm to inch,
+# 2. km to miles,
+# 3. USD to INR,
+# 4. exit.
+
+user_input = input('''
+Hi! Welcome to my page
+What would you like to do?
