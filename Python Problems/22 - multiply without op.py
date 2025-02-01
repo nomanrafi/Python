4baeecae1b280231b7954ac74142c869c64bd684
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-01 22:48:09 +0600

    Update 22 - multiply without op.py

---
+++ Python Problems/22 - multiply without op.py
@@ -0,0 +1,6 @@
+# Write a program that can multiply two numbers provided by the user without using the * operator.
+
+first_num = int(input("Enter your first number: "))
+second_num = int(input("Enter your second number: "))
+
+sum = 0
