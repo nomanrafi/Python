016a792ddd32c41606f78be517a73d0dcb12299f
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-01 22:48:26 +0600

    Update 22 - multiply without op.py

--- Python Problems/22 - multiply without op.py
+++ Python Problems/22 - multiply without op.py
@@ -4,3 +4,8 @@ first_num = int(input("Enter your first number: "))
 second_num = int(input("Enter your second number: "))
 
 sum = 0
+
+for i in range(0, second_num):
+    sum = sum + first_num
+
+print("Your result is: ", sum)
