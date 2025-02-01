513c3bb5915c190c953496c7020c8fda1e725df0
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-01 22:49:05 +0600

    Update 23 - factorial.py

--- Python Problems/23 - factorial.py
+++ Python Problems/23 - factorial.py
@@ -3,3 +3,10 @@
 num = int(input("Enter your number: "))
 
 i = 1
+if num > 0:
+    while num >= 1:
+        i = i * num
+        num = num - 1
+    print("Factorial of the given number is: ", i)
+else:
+    print("Factorial not possible")
