0b437d5f890ae11af1f34e1fe43c717e3eee57ed
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-03 23:36:27 +0600

    Update 27 - population calc.py

---
+++ Python Problems/27 - population calc.py
@@ -0,0 +1,8 @@
+# The current population of a town is 10,000.
+# The population of the town is increasing at a rate of 10% per year.
+# You have to write a program to find out the population at the end of each of the last 10 years.
+
+# For example, if the current population is 10,000, the output should be like this:
+# 10th year - 10,000
+# 9th year - 9,000
+# 8th year - 8,100, and so on.
