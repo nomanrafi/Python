9974180e873e17a3faceba3c36512e5655243f83
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-03 23:38:40 +0600

    Update 30 - calc lcm.py

---
+++ Python Problems/30 - calc lcm.py
@@ -0,0 +1,6 @@
+# The user will provide two numbers, and you have to find the LCM of those two numbers.
+
+a = int(input('Enter your first number: '))
+num_1 = a
+b = int(input('Enter your second number: '))
+num_2 = b
