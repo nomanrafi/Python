b7ba44559f83c2fb4ddb877bac6891ab68d31b8c
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-04 23:51:41 +0600

    Update 33 - compound interest.py

--- Python Problems/33 - compound interest.py
+++ Python Problems/33 - compound interest.py
@@ -3,3 +3,6 @@
 p = int(input('Enter your principal: '))
 r = int(input('Enter rate of interest: '))
 t = int(input('Enter the time period elapsed: '))
+
+a = p * (1 + r / 100)**t
+print('Your amount is:', a)
