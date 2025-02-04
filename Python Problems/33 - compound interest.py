91e7cddb493777de85a9d07b4ae4883809e65e10
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-04 23:51:33 +0600

    Update 33 - compound interest.py

---
+++ Python Problems/33 - compound interest.py
@@ -0,0 +1,5 @@
+# Write a program to calculate compound interest.
+
+p = int(input('Enter your principal: '))
+r = int(input('Enter rate of interest: '))
+t = int(input('Enter the time period elapsed: '))
