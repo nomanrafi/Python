ed761655b008c93b52db30900ce0230baa645957
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-04 23:49:31 +0600

    Update 31 - first 25 primes.py

--- Python Problems/31 - first 25 primes.py
+++ Python Problems/31 - first 25 primes.py
@@ -7,4 +7,7 @@ while counter <= 25:
     for i in range(2, num):
         if num % i == 0:
             break
-        
+        else:
+            print(num)
+            counter = counter + 1
+        num = num + 1
