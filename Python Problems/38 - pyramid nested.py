1a4d2c92d7fc82e9572f15bf8f57c60f2199b9e2
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-06 22:50:00 +0600

    Update 38 - pyramid nested.py

--- Python Problems/38 - pyramid nested.py
+++ Python Problems/38 - pyramid nested.py
@@ -5,3 +5,10 @@
 # * * *
 
 row = int(input('Enter number of rows: '))
+for i in range(0, row):
+    for j in range(0, row - i - 1):
+        print(end=' ')
+    for k in range(0, i + 1):
+        print('*', end=' ')
+    print()
+    
