790b8707aa4cc2436f7080c49d44019492d437e9
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-07 21:58:04 +0600

    Update 40 - sum series.py

---
+++ Python Problems/40 - sum series.py
@@ -0,0 +1,7 @@
+# Write a program to calculate the sum of the following series up to the nth term:
+
+# 1/1! + 2/2! + 3/3! + 4/4! + ... + n/n!
+
+# The value of n will be provided by the user.
+
+num = int(input('Enter your number: '))
