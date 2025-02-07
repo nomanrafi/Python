d6381cc51da6daad3b5437d07ecc1e1120385d6d
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-07 21:56:47 +0600

    Update 39 - patterns nested.py

--- Python Problems/39 - patterns nested.py
+++ Python Problems/39 - patterns nested.py
@@ -18,3 +18,13 @@ for i in range(1, row + 1):
 # *
 
 row = int(input('Enter number of rows: '))
+for i in range(1, row + 1):
+    for j in range(0, i):
+        print('*', end=' ')
+    print('')
+
+for k in range(row, 0, -1):
+    for l in range(0, k - 1):
+        print('*', end=' ')
+    print('')
+    
