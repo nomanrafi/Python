15244e12bd6f2072ef003ec392747b4e66f31114
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-08 21:10:35 +0600

    Update 41 - series sum.py

--- Python Problems/41 - series sum.py
+++ Python Problems/41 - series sum.py
@@ -11,4 +11,10 @@ sum = 1
 
 for i in range(2, n + 1):
     sum = sum + ((x**i)/i)
-    
+print(sum)
+
+# The natural logarithm can be approximated by the following series:
+
+# (x-1)/x + 1/2 * ((x-1)/x)^2 + 1/2 * ((x-1)/x)^3 + ... + 1/2 * ((x-1)/x)^7
+
+# If x is input through the keyboard, write a program to calculate the sum of the first seven terms of this series.
