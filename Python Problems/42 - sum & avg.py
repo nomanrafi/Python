31521352060a5519ec78303b181eae209a70d546
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-08 21:12:01 +0600

    Update 42 - sum & avg.py

---
+++ Python Problems/42 - sum & avg.py
@@ -0,0 +1,8 @@
+# Write a program that continues to accept a number from the user until the user enters zero.
+# Display the sum and average of all the numbers.
+
+num = int(input('Enter your number: '))
+
+add = 0
+avg = 0
+count = 0
