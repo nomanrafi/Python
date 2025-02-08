fdb92eb316754d750c8b3aea1a727c8d5ed5511f
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-08 21:10:08 +0600

    Update 41 - series sum.py

---
+++ Python Problems/41 - series sum.py
@@ -0,0 +1,8 @@
+# Write a program to find the sum of the series up to the nth term:
+
+# 1 + x^2/2 + x^3/3 + ... + x^n/n
+
+# The value of n will be provided by the user.
+
+x = int(input('Enter your number: '))
+n = int(input('Enter nth value: '))
