23b916445fba72c42341c87e35add3b715810e22
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-10 00:00:11 +0600

    Update 45 - username from email.py

--- Python Problems/45 - username from email.py
+++ Python Problems/45 - username from email.py
@@ -1,3 +1,5 @@
 # Extract the username from a given email.
 # For example, if the email is saurabh@gmail.com, then the username should be saurabh.
 email = input('Enter your email: ')
+username, domain = email.split('@')
+print(username)
