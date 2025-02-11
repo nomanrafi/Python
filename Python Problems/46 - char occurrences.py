bf33bfdcb2be1289430f73d636651b337dbfe2bc
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-11 21:28:54 +0600

    Update 46 - char occurrences.py

---
+++ Python Problems/46 - char occurrences.py
@@ -0,0 +1,5 @@
+# Count the frequency of a particular character in a provided string.
+# For example, in the string 'hello, how are you,' the frequency of 'h' is 2.
+
+a = input('Enter your string: ')
+b = input('Enter the character: ')
