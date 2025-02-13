d714763f5c2ccab389ffccf53938bdfa74e8c993
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-13 22:59:50 +0600

    Update 60 - sep even-odd list.py

---
+++ Python Problems/60 - sep even-odd list.py
@@ -0,0 +1,7 @@
+# Create two lists from a given list where the first list will contain all the odd numbers from the original list, and the second one will contain all the even numbers.
+
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+L_even = []
+L_odd = []
+
