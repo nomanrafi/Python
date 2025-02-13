bb9785a6496f41312ecbfd7fd688215661725969
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-13 22:59:18 +0600

    Update 59 - list sorted without sort().py

---
+++ Python Problems/59 - list sorted without sort().py
@@ -0,0 +1,10 @@
+# Write a program to check whether a list is in ascending order or not.
+
+L = [2, 3, 1, 6, 0]
+flag = 0
+
+for i in range(0, len(L)-1):
+    if L[i] >= L[i+1]:
+        flag = 1
+        break
+    
