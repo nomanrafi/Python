34370522475234889168f8e4746c163616bf4a46
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-13 22:58:05 +0600

    Update 57 - reverse words.py

---
+++ Python Problems/57 - reverse words.py
@@ -0,0 +1,7 @@
+# Write a program that can reverse the words of a given string.
+# For example, if the input is 'Hello how are you',
+# the output should be 'you are how Hello'.
+
+a = input('Enter your string: ')
+x = a.split()
+rev = []
