408ef977cdfd8c0314746462194ee29e6399b507
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-14 22:17:45 +0600

    Update 62 - replace item.py

--- Python Problems/62 - replace item.py
+++ Python Problems/62 - replace item.py
@@ -1,3 +1,6 @@
 # Write a program to replace an item with a different item if it is found in the list.
 
 L = [1, 2, 3, 4]
+# Write a program to replace an item with a different item if it is found in the list.
+
+L = [1, 2, 3, 4]
