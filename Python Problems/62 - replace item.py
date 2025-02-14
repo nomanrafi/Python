8527d38b5e49284ad71724c4ca6fb92511a451fe
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-14 22:17:56 +0600

    Update 62 - replace item.py

--- Python Problems/62 - replace item.py
+++ Python Problems/62 - replace item.py
@@ -4,3 +4,9 @@ L = [1, 2, 3, 4]
 # Write a program to replace an item with a different item if it is found in the list.
 
 L = [1, 2, 3, 4]
+
+for i in range(0, len(L)):
+    if find == L[i]:
+        L[i] = replace
+        print(L)
+        break
