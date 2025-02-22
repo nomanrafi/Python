88e947272cb80f2712f10ca2cbb400c3252e0d15
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-23 01:38:15 +0600

    Update 68 - sort list without sort().py

--- Python Problems/68 - sort list without sort().py
+++ Python Problems/68 - sort list without sort().py
@@ -5,4 +5,6 @@ L = [5, 2, 3, 6, 4, 7]
 for i in range(len(L)):
     for k in range(0, len(L)-1):
         if L[k] > L[k+1]:
-            
+            temp = L[k]
+            L[k] = L[k+1]
+            L[k+1] = temp
