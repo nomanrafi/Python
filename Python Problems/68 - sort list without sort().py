9755ce483752b34644331c870977448f2fb9cfb4
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-23 01:38:05 +0600

    Update 68 - sort list without sort().py

--- Python Problems/68 - sort list without sort().py
+++ Python Problems/68 - sort list without sort().py
@@ -1,3 +1,8 @@
 # Write a program that can sort a given unsorted list.
 # Don't use any built-in function for sorting.
 L = [5, 2, 3, 6, 4, 7]
+
+for i in range(len(L)):
+    for k in range(0, len(L)-1):
+        if L[k] > L[k+1]:
+            
