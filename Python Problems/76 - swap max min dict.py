074760ee1212c3bc24a4f7faa66101ef934caaed
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-25 23:37:50 +0600

    Update 76 - swap max min dict.py

--- Python Problems/76 - swap max min dict.py
+++ Python Problems/76 - swap max min dict.py
@@ -2,3 +2,6 @@
 # For example, if the dictionary is like this {‘a’: 1, ‘b’: 2, ‘c’: 3}:
 # The output should be {a: 3, b: 2, c: 1}.
 D = {'a': 100, 'b': 200, 'c': 300, 'd': 400}
+
+max_val = max(D.values())
+min_val = min(D.values())
