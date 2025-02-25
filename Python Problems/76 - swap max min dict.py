05f6fe4cb49afdea628cbdfebdc85ebef6fe5897
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-02-25 23:37:29 +0600

    Update 76 - swap max min dict.py

---
+++ Python Problems/76 - swap max min dict.py
@@ -0,0 +1,3 @@
+# Write a program to swap the key-value pairs for the maximum and minimum values.
+# For example, if the dictionary is like this {‘a’: 1, ‘b’: 2, ‘c’: 3}:
+# The output should be {a: 3, b: 2, c: 1}.
